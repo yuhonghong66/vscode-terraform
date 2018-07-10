4c2f102275523078cba603283f180538d087f33f
Mikael Olenfalk <mikael@olenfalk.se>
2018-07-10 22:23:18 +0200

    Add data store for load completion data

---
+++ src/autocompletion/completion-data.ts
@@ -0,0 +1,166 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+export interface ProviderInfo {
+  provider_arguments: any[];
+  resources: { [name: string]: SectionInfo };
+  datas: { [name: string]: SectionInfo }
+}
+
+export interface SectionInfo {
+  arguments: SubblockInfo[] | ArgumentInfo[];
+  attributes: AttributeInfo[];
+}
+
+export interface ArgumentInfo {
+  word: string;
+  type: "bool" | "float" | "int" | "list" | "map" | "set" | "string";
+  required: boolean;
+  block: false;
+}
+
+export interface SubblockInfo {
+  word: string;
+  type: "bool" | "float" | "int" | "list" | "map" | "set" | "string";
+  required: boolean;
+  block: true;
+  subblock: SubblockInfo[] | ArgumentInfo[];
+}
+
+export interface AttributeInfo {
+  word: string;
+  type: "bool" | "float" | "int" | "list" | "map" | "set" | "string";
+  block: false;
+}
+
+interface RawCompletionIndex {
+  providers: { [name: string]: ProviderIndex };
+  views: Views;
+}
+
+export interface ProviderIndex {
+  versions: { [version: string]: VersionOverview };
+  meta: ProviderMetadata;
+  latest: string;
+}
+
+export interface VersionOverview {
+  path: string;
+  resources: string[];
+  datas: string[];
+  unknowns: string[];
+}
+
+export interface ProviderMetadata {
+  type: "provider" | "community_provider";
+}
+
+export interface Views {
+  all: {
+    resources: string[];
+    datas: string[];
+    unknowns: string[];
+  }
+}
+
+function loadJsonAs<T>(jsonPath: string): Promise<T> {
+  return new Promise<T>((resolve, reject) => {
+    fs.readFile(jsonPath, (err, data) => {
+      if (err) {
+        reject(new Error(`Could not load data from ${jsonPath}: ${err}`));
+      } else {
+        const json = JSON.parse(data.toString());
+        resolve(json as T);
+      }
+    });
+  });
+}
+
+export class CompletionIndex {
+  private knownProviders: string[];
+
+  private constructor(private data: RawCompletionIndex) {
+    this.knownProviders = Object.keys(this.data.providers);
+  }
+
+  static async create(indexPath: string): Promise<CompletionIndex> {
+    return new CompletionIndex(await loadJsonAs<RawCompletionIndex>(indexPath));
+  }
+
+  get providers() {
+    return this.knownProviders;
+  }
+
+  provider(name: string): ProviderIndex {
+    return this.data.providers[name];
+  }
+
+  all(type: "resource" | "data", prefix?: string): string[] {
+    const all = type === "resource" ? this.data.views.all.resources : this.data.views.all.datas;
+
+    if (!prefix)
+      return all;
+
+    return all.filter((i) => i.startsWith(prefix));
+  }
+
+  realVersion(provider: string, version: string = "LATEST"): string {
+    if (version !== "LATEST")
+      return version;
+
+    const p = this.provider(provider);
+    if (!p) {
+      throw new Error(`Unknown provider ${provider}`);
+    }
+
+    return p.latest;
+  }
+}
+
+export class CompletionData {
+  private _loadedData = new Map<string, Map<string, ProviderInfo>>();
+
+  private constructor(private _basePath: string, private _index: CompletionIndex) {}
+
+  static async create(basePath: string): Promise<CompletionData> {
+    const indexPath = path.join(basePath, "provider-index.json");
+    return new CompletionData(basePath, await CompletionIndex.create(indexPath));
+  }
+
+  get index(): CompletionIndex {
+    return this._index;
+  }
+
+  get loadedProviders(): string[] {
+    return [...this._loadedData.keys()];
+  }
+
+  async load(provider: string, version: string = "LATEST"): Promise<ProviderInfo> {
+    const realVersion = this.index.realVersion(provider, version);
+
+    const providerIndexData = this.index.provider(provider);
+    if (!providerIndexData) {
+      throw new Error(`Unknown provider ${provider}`);
+    }
+
+    const providerVersionInfoData = providerIndexData.versions[realVersion];
+    if (!providerVersionInfoData) {
+      throw new Error(`Unknown provider version ${realVersion} (requested: ${version}) of provider ${provider}`);
+    }
+
+    let providerData = this._loadedData.get(provider);
+    if (!providerData) {
+      providerData = new Map<string, ProviderInfo>();
+      this._loadedData.set(provider, providerData);
+    }
+
+    let providerInfo = providerData.get(realVersion);
+    if (!providerInfo) {
+      const dataPath = path.join(this._basePath, "providers", providerVersionInfoData.path);
+      providerInfo = await loadJsonAs<ProviderInfo>(dataPath);
+      providerData.set(realVersion, providerInfo);
+    }
+
+    return providerInfo;
+  }
+}
---
+++ test/autocompletion/completion-data.unit.test.ts
@@ -0,0 +1,84 @@
+import * as assert from "assert";
+import * as path from "path";
+import { CompletionData, CompletionIndex } from "../../src/autocompletion/completion-data";
+
+suite("Autocompletion Data", () => {
+  suite("CompletionIndex", () => {
+    test("create", async () => {
+      let index = await CompletionIndex.create(path.resolve(__dirname, "../../out/src/data/provider-index.json"));
+
+      assert(index);
+      assert(index.providers.length > 100, `Not enough providers load from index. It should be around >120, but only ${index.providers.length} found.`);
+    });
+
+    test("ensure common providers exist", async () => {
+      let index = await CompletionIndex.create(path.resolve(__dirname, "../../out/src/data/provider-index.json"));
+
+      for (const name of ['aws', 'azurerm', 'google']) {
+        assert(index.provider("aws"), `could not find common provider: ${name}`);
+      }
+    });
+
+    test("all can return all resources", async () => {
+      let index = await CompletionIndex.create(path.resolve(__dirname, "../../out/src/data/provider-index.json"));
+
+      let resources = index.all("resource");
+      assert(resources.length > 200);
+    });
+
+    test("all can return all datas", async () => {
+      let index = await CompletionIndex.create(path.resolve(__dirname, "../../out/src/data/provider-index.json"));
+
+      let datas = index.all("data");
+      assert(datas.length > 200);
+    });
+
+    test("all filters by prefix", async () => {
+      let index = await CompletionIndex.create(path.resolve(__dirname, "../../out/src/data/provider-index.json"));
+
+      let resources = index.all("resource", "azurerm");
+      for (const resource of resources) {
+        assert(resource.startsWith("azurerm"), `${resource} was expected to start with 'azurerm'`);
+      }
+
+      let datas = index.all("data", "azurerm");
+      for (const data of datas) {
+        assert(data.startsWith("azurerm"), `${data} was expected to start with 'azurerm'`);
+      }
+    });
+  });
+
+  suite("CompletionData", () => {
+    test("create", async () => {
+      let cd = await CompletionData.create(path.resolve(__dirname, "../../out/src/data"));
+
+      assert(cd);
+      assert(cd.index);
+      assert.equal(cd.loadedProviders.length, 0);
+    });
+
+    test("load returns a provider", async () => {
+      let cd = await CompletionData.create(path.resolve(__dirname, "../../out/src/data"));
+
+      let pi = await cd.load("aws");
+      assert(pi);
+      assert.equal(cd.loadedProviders.length, 1);
+    });
+
+    test("load returns latest version by default", async () => {
+      let cd = await CompletionData.create(path.resolve(__dirname, "../../out/src/data"));
+
+      let pi1 = await cd.load("aws");
+      let pi2 = await cd.load("aws", "LATEST");
+      assert.deepEqual(pi1, pi2);
+    });
+
+    test("load returns a specific provider version if requested", async () => {
+      let cd = await CompletionData.create(path.resolve(__dirname, "../../out/src/data"));
+
+      let pi1 = await cd.load("aws", "1.25.0");
+      let pi2 = await cd.load("aws", "LATEST");
+      assert.notDeepEqual(pi1, pi2);
+    });
+  });
+});
